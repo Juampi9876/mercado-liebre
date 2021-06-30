c29064c2215ed1692dc2acb85899560776855a2d
Juampi <84481703+Juampi9876@users.noreply.github.com>
2021-06-30 00:25:01 -0300

    x

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const publicPath = path.resolve('public');
 
 app.set("port",process.env.PORT || 3000)
 
+app.listen(app.get("port"),() => console.log("Server Start http://localhost:"+app.get("port")))
+
 app.use(express.static(publicPath));
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -28,8 +30,6 @@ app.get('/login.html', function(req, res) {
 app.post('/login', function(req, res) {
     res.send(req.body);
 });
-
-app.listen(app.get("port"),() => console.log("Server Start http://localhost:"+app.get("port")))
 
 app.listen(process.env.PORT || 3000, function(){
     console.log('Servidor Corriendo en el puerto 3000')
